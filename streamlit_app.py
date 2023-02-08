2e19a126d724122447709bbf8ad327aa9516aa6a
Dhars169 <124091114+Dhars169@users.noreply.github.com>
2023-02-08 10:12:21 +0530

    Update streamlit_app.py
    Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,11 +48,16 @@ streamlit.stop()
 
 #import snowflake.connector
 
+streamlit.header("fruit load list contains:")
+#snowflake - related functions
+def get_fruit_load_list():
+     with my_cux.cursor() as my_cur:
+     my_cur.execute("select * from fruit_load_list")
+     return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
+my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
